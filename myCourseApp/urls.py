a421f4f16adbd082735d40446c7f140200f7066b
Cyngith33 <c2024304@hallam.shu.ac.uk>
2023-07-19 14:49:14 +0000

    User Registration View and url link, Changed base template title to module

--- myCourseApp/urls.py
+++ myCourseApp/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from modules import views
+from users.views import register
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about', views.about, name = 'about'),
+    path('register/', views.register, name='register')
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
+from users.models import Student
+from .forms import UserRegisterForm, UserProfileForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        username = form.data['username']
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+            
+            # Check if a profile already exists for the user
+            profile, created = Student.objects.get_or_create(user=user)
+            
+            # Update the profile fields
+            profile.image = profile_form.cleaned_data['image']
+            profile.date_of_birth = datetime.fromisoformat(str(profile_form.cleaned_data['date_of_birth']))
+            profile.address = profile_form.cleaned_data['address']
+            profile.city = profile_form.cleaned_data['city']
+            profile.country = profile_form.cleaned_data['country']
+            profile.secret_question = profile_form.cleaned_data['secret_question']
+            profile.secret_answer = profile_form.cleaned_data['secret_answer']
+            
+            # Save the department name
+            selected_department = profile_form.cleaned_data['department']
+            profile.department = selected_department.name if selected_department else ''
+            profile.save()
+            
+            messages.success(request, f'Your account has been created! Now you can login!')
+            return redirect('login')
+        else:
+            messages.warning(request, f'Unable to create an account for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+        profile_form = UserProfileForm()
+        return render(request, 'register.html', {'form': form, 'profile_form': profile_form, 'title': 'Student Registration'})
+
+
 # Create your views here.
