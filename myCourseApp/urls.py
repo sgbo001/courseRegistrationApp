531053c11f65aa7f525c5a99f23e926cda43cd5d
Cyngith33 <c2024304@hallam.shu.ac.uk>
2023-07-21 20:50:12 +0000

    Logout url pattern

--- myCourseApp/urls.py
+++ myCourseApp/urls.py
@@ -5,9 +5,12 @@ from modules import views
 from users.views import register
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about', views.about, name = 'about'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login', auth_views.LogoutView.as_view(template_name = 'login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name = 'logout.html'), name='logout')
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
