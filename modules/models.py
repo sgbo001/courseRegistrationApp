9a3e8005e0c39b30bd09eedeed30157a08ee7a77
metobi1 <metobi05@gmail.com>
2023-07-22 11:04:28 +0000

    Added user profile template, course model and registered the course model

--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
-# Create your models here.
+
+class Course(models.Model):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE)
+    code = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(default='course_pics/logo.jpg', upload_to='course_pics')
+    description = models.TextField(blank=True, null=True)
+    duration = models.CharField(max_length=255, blank=True, null=True)
+    department = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.group.name
