3f9769ae1304e5ccda862b687b045828a632d467
sgbo001 <olaide.adetoyese@gmail.com>
2023-08-11 14:14:51 +0000

    code refactor

--- myCourseApp/storage.py
+++ myCourseApp/storage.py
@@ -1,19 +1,14 @@
 
-
-
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-from django.conf import settings
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
-    account_name = settings.AZURE_ACCOUNT_NAME
-    account_key = settings.AZURE_ACCOUNT_KEY
-    azure_container = settings.AZURE_MEDIA_CONTAINER
+    account_name = 'c2063081' # Must be replaced by your <storage_account_name>
+    account_key = 'Ce8GO0ugoRfwDfGgE12t9gmwdJt7fUQquK00mQD57Xm00PSMSBY1jJwAel5dqDaWjp3nJ3b1SjVd+AStLO6D1A==' 
+    azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = settings.AZURE_ACCOUNT_NAME
-    account_key = settings.AZURE_ACCOUNT_KEY
-    azure_container = settings.AZURE_STATIC_CONTAINER
+    account_name = 'c2063081' 
+    account_key = 'Ce8GO0ugoRfwDfGgE12t9gmwdJt7fUQquK00mQD57Xm00PSMSBY1jJwAel5dqDaWjp3nJ3b1SjVd+AStLO6D1A==' 
+    azure_container = 'static'
     expiration_secs = None
-
