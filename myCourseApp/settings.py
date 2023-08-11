2516501fb2f0197f95a68d1ad35e42e058051486
Adetoyese Olaide <82801465+sgbo001@users.noreply.github.com>
2023-08-11 14:32:09 +0100

    Update settings.py

--- myCourseApp/settings.py
+++ myCourseApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from azure.storage.blob import AzureBlobStorage
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
